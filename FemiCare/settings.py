0ae7fed994a2b0c8c9a47eb34609d0ee4b67d2f2
Jesika Raut <jsscraut@gmail.com>
2026-04-14 23:43:29 +0545

    resolve HTTPS redirect loop by adding SECURE_PROXY_SSL_HEADER

--- FemiCare/settings.py
+++ FemiCare/settings.py
@@ -60,6 +60,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEBUG = False  # local
 
 if not DEBUG:
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
