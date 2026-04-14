8e3b3e29d0c28c57fd113b67bddb55e82708f4f7
Jesika Raut <jsscraut@gmail.com>
2026-04-14 23:28:25 +0545

    asgi.py and start.sh fixed for deployment badgateway

--- FemiCare/asgi.py
+++ FemiCare/asgi.py
@@ -1,22 +1,21 @@
-"""
-ASGI config for FemiCare project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
+import django
 from django.core.asgi import get_asgi_application
+
+# Set the settings module environment variable
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FemiCare.settings')
+
+# Initialize the Django ASGI application first
+django_asgi_app = get_asgi_application()
+
+# NOW  import your channels-specific modules
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import tracker.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FemiCare.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    # Use the initialized django_asgi_app here
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             tracker.routing.websocket_urlpatterns
